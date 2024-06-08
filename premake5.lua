a786c482ae87b5a9bc742cd69bd6424bac1114f3
odysseia06 <floydianayhan@gmail.com>
2024-06-08 10:53:58 +0300

    build with GLFW

--- premake5.lua
+++ premake5.lua
@@ -2,16 +2,23 @@ workspace "limbo"
 	architecture "x64"
 	configurations { "Debug", "Release" }
 	location "build"
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "vendor/glfw/include"
+include "vendor/GLFW"
 
 project "limbo"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++20"
-	targetdir "bin/%{cfg.buildcfg}"
+	targetdir ("bin/" .. outputdir .. "%/{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	pchheader "lmbpch.h"
 	pchsource "src/lmbpch.cpp"
 	files { "src/**.h", "src/**.cpp", "main/**.cpp" }
-	includedirs { "src" }
+	includedirs { "src", "%{IncludeDir.GLFW}" }
+	links { "GLFW", "opengl32.lib" }
 	filter "configurations:Debug"
 		defines { "L_DEBUG" }
 		symbols "On"
@@ -24,15 +31,16 @@ project "gtest"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
-	targetdir "bin/%{cfg.buildcfg}"
+	targetdir ("bin/" .. outputdir .. "%/{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	files { "vendor/googletest/googletest/src/gtest-all.cc", "vendor/googletest/googlemock/src/gmock-all.cc" }
 	includedirs { "vendor/googletest/googletest/include", "vendor/googletest/googletest/src", "vendor/googletest/googletest",
 				  "vendor/googletest/googlemock/include", "vendor/googletest/googlemock/src", "vendor/googletest/googlemock"}
 	filter "configurations:Debug"
-		defines { "DEBUG" }
+		defines { "L_DEBUG" }
 		symbols "On"
 	filter "configurations:Release"
-		defines { "NDEBUG" }
+		defines { "L_RELEASE" }
 		optimize "On"
 
 -- Unit tests project
@@ -40,13 +48,14 @@ project "tests"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++20"
-	targetdir "bin/%{cfg.buildcfg}"
+	targetdir ("bin/" .. outputdir .. "%/{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	files { "tests/**.h", "tests/**.cpp" }
 	includedirs { "src", "vendor/googletest/googletest/include", "vendor/googletest/googlemock/include" }
 	links { "limbo", "gtest" }
 	filter "configurations:Debug"
-		defines { "DEBUG" }
+		defines { "L_DEBUG" }
 		symbols "On"
 	filter "configurations:Release"
-		defines { "NDEBUG" }
+		defines { "L_RELEASE" }
 		optimize "On"
